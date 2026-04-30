8b9664146f333a83adc43768a633fd7e3268933d
Safaa Ettalhi <safaeettalhi@gmail.com>
2026-04-30 21:05:09 +0100

    v mobile

---
+++ android/app/src/main/java/com/lesartistes/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.lesartistes.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lesartistes.app',
+  appName: 'Les Artistes',
+  webDir: 'out'
+};
+
+export default config;
